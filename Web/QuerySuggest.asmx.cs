08df82acdad04014e4b3ff0e52ebe6b5c60e7989
campbellsean <seanpc@uw.edu>
2018-03-06 00:02:17 -0800

    pa2 fully implmented

--- Web/QuerySuggest.asmx.cs
+++ Web/QuerySuggest.asmx.cs
@@ -20,7 +20,7 @@ namespace Web
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+     [System.Web.Script.Services.ScriptService]
     public class QuerySuggest : System.Web.Services.WebService
     {
         private static CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
